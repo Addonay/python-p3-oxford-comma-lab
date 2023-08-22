5d70eb861f579fd3ff7695ecfa7dd8bdfb52650f
Addonay <addonay.osoro@student.moringaschool.com>
2023-08-22 18:40:12 +0300

    solved lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,9 @@
 def oxford_comma(items):
-    return None
+    if len(items) <= 2:
+        return " and ".join(items)
+    else:
+        last_element = "and " + items[-1]
+        other_items = ", ".join(items[:-1])
+        return other_items + ", " + last_element
+items = ["fiddleheads", "okra", "kohlrabi"]
+print(oxford_comma(items))
